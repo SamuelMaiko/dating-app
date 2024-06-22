863ade12e7d80562706a12136dea9d7fee535bd1
Samuel Maiko <sammymaiko721@gmail.com>
2024-06-22 12:42:44 +0300

    ready for depl

--- profiles/serializers/FavoriteSerializer.py
+++ profiles/serializers/FavoriteSerializer.py
@@ -7,13 +7,17 @@ class FavoriteSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='favorite.username')
     profile_picture = serializers.SerializerMethodField()
     date_of_birth = serializers.SerializerMethodField()
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Favorite
-        fields = ['user_id', 'username', 'profile_picture', 'date_of_birth']
+        fields = ['user_id', 'username', 'profile_picture','age', 'date_of_birth']
 
     def get_profile_picture(self, obj):
         return obj.favorite.user_profile.profile_picture.url if obj.user.user_profile.profile_picture else None
 
     def get_date_of_birth(self, obj):
         return obj.favorite.user_profile.date_of_birth if obj.user.user_profile.date_of_birth else None
+    
+    def get_age(self, obj):
+        return obj.favorite.user_profile.age if obj.user.user_profile.date_of_birth else None
